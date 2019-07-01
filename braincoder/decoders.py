26114f5fdac7a37f8959b880af409b96d1c9032a
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2019-07-01 21:23:39 +0200

    Some tweaks. Very important to make paradigm tf.constant rather than tf.Variable)

--- braincoder/decoders.py
+++ braincoder/decoders.py
@@ -7,77 +7,152 @@ import pandas as pd
 
 class EncodingModelFitter(object):
 
-    def __init__(self,
-                 paradigm,
-                 data):
-
-        assert(len(data) == len(paradigm))
-
-        self.n_features = data.shape[1]
-        self.n_timepoints = data.shape[0]
-
-        self.data = pd.DataFrame(data)
-        self.paradigm = np.atleast_2d(paradigm).T
-
-        self.graph = tf.Graph()
-
-        with self.graph.as_default():
-            self.parameters = tf.Variable(np.ones((1,
-                                                   self.n_parameters,
-                                                   self.n_features)))
+    def __init__(self):
+        pass
 
     def optimize(self,
-                 min_nsteps=1000,
+                 paradigm,
+                 data,
+                 min_nsteps=100000,
                  ftol=1e-9,
                  ):
 
-        ftol_ = 1 + ftol
+        assert(len(data) == len(paradigm))
+
+        paradigm = paradigm.astype(np.float32)[:, np.newaxis]
+        data = pd.DataFrame(data.astype(np.float32))
+
+        init_pars = self.init_parameters(data.values, paradigm[:, 0])
+
+        self.init_graph(paradigm, init_pars)
 
         with self.graph.as_default():
-            init = tf.global_variables_initializer()
-            with tf.Session() as session:
-                session.run(init)
 
+            self.cost = tf.reduce_sum((data.values - self.predictions)**2)
+
+            optimizer = tf.train.AdamOptimizer()
+
+            train = optimizer.minimize(self.cost)
+
+            init = tf.global_variables_initializer()
+
+            with tf.Session() as session:
                 costs = np.ones(min_nsteps) * np.inf
 
+                _ = session.run([init])
+
                 pbar = tqdm(range(min_nsteps))
+
+                ftol_ratio = 1 + ftol
                 for step in pbar:
-                    _, c = session.run([self.train, self.cost])
+                    _, c, p = session.run([train, self.cost, self.parameters])
                     costs[step] = c
                     pbar.set_description(f'Current cost: {c:7g}')
 
-                    if (costs[step - 1] >= c) & (costs[step - 1] / c < ftol_):
+                    if (costs[step - 1] >= c) & (costs[step - 1] / c < ftol_ratio):
+                        print(session.run(self.parameters))
                         break
 
+                parameters, predictions = session.run([self.parameters, self.predictions])
+
                 costs = pd.Series(costs[:step + 1])
-                parameters, predictions = session.run(
-                    [self.parameters, self.predictions])
                 parameters = pd.DataFrame(np.squeeze(parameters),
                                           index=self.parameter_labels,
-                                          columns=self.data.columns)
+                                          columns=data.columns)
+                print(parameters)
 
                 predictions = pd.DataFrame(predictions,
-                                           index=self.data.index,
-                                           columns=self.data.columns)
+                                           index=data.index,
+                                           columns=data.columns)
 
             session.close()
             return costs, parameters, predictions
 
+    def simulate(self, parameters, paradigm, noise=1.):
+        """
+        * Parameters should be an array of size M or (M, N),
+        where M is the number of parameters and N the number of
+        parameter sets.
+        * Paradigm should be an array of size N, where N
+        is the number of timepoints
+
+        """
+
+        paradigm = pd.DataFrame(paradigm.astype(np.float32))
+
+        if parameters.ndim == 1:
+            parameters = parameters[np.newaxis, :, np.newaxis]
+        elif parameters.ndim == 2:
+            parameters = parameters.T[np.newaxis, ...]
+
+        parameters = parameters.astype(np.float32)
+
+        self.init_graph(paradigm, parameters)
+
+        with self.graph.as_default():
+
+            # noise = tf.random_normal(shape=(paradigm.shape[0],
+                                            # parameters.shape[2]),
+                                     # mean=0.0,
+                                     # stddev=noise,
+                                     # dtype=tf.float32)
+
+            # noisy_prediction = self.predictions + noise
+            noisy_prediction = self.predictions
+
+            with tf.Session() as session:
+                self.parameters.load(parameters, session)
+                predictions_ = session.run(noisy_prediction)
+
+            session.close()
+
+            predictions_ += np.random.randn(*predictions_.shape) * noise
+
+        return pd.DataFrame(predictions_)
+
+    def init_graph(self, paradigm, parameters=None):
+
+        self.graph = tf.Graph()
+
+        with self.graph.as_default():
+            self.parameters = tf.Variable(parameters.astype(np.float32),
+                                          name='parameters')
+            self.paradigm = tf.constant(paradigm.astype(np.float32),
+                                        name='paradigm')
+
 
 class GaussianReceptiveFieldFitter(EncodingModelFitter):
 
     n_parameters = 4
     parameter_labels = ['mu', 'sd', 'amplitude', 'baseline']
 
-    def __init__(self,
+    def __init__(self, positive_amplitudes=True):
+
+        super().__init__()
+        self.positive_amplitudes = positive_amplitudes
+
+    def optimize(self,
                  paradigm,
                  data,
-                 positive_amplitudes=True):
+                 min_nsteps=100000,
+                 ftol=1e-6):
 
-        super().__init__(paradigm,
-                         data)
+        costs, parameters, predictions = super().optimize(paradigm,
+                                                          data,
+                                                          min_nsteps,
+                                                          ftol)
 
-        self.positive_amplitudes = positive_amplitudes
+        parameters.loc['sd'] = _softplus(parameters.loc['sd'])
+
+        if self.positive_amplitudes:
+            parameters.loc['amplitude'] = _softplus(
+                parameters.loc['amplitude'])
+
+        return costs, parameters, predictions
+
+    def init_graph(self, paradigm, parameters=None):
+
+        super().init_graph(paradigm, parameters)
 
         with self.graph.as_default():
             self.mu = self.parameters[:, 0, :]
@@ -89,75 +164,51 @@ class GaussianReceptiveFieldFitter(EncodingModelFitter):
             else:
                 self.amplitude = self.parameters[:, 2, :]
             self.baseline = self.parameters[:, 3, :]
-            self.norm = tfd.Normal(self.mu, self.sd)
+
             self.predictions = self.baseline + \
-                self.norm.prob(self.paradigm) / \
-                self.norm.prob(self.mu) * self.amplitude
+                norm(self.paradigm, self.mu, self.sd) * \
+                self.amplitude
 
-            self.cost = tf.reduce_sum((self.predictions - self.data.values)**2)
+    def init_parameters(self, data, paradigm):
+        baselines = data.min(0)
+        data_ = data - baselines
+        mus = (data_ * paradigm[:, np.newaxis]).sum(0) / data_.sum(0)
+        sds = (data_ * (paradigm[:, np.newaxis] -
+                        mus[np.newaxis, :])**2).sum(0) / data_.sum(0)
+        sds = np.sqrt(sds)
+        amplitudes = data_.max(0)
 
-            self.optimizer = tf.train.AdamOptimizer()
-            self.train = self.optimizer.minimize(self.cost)
+        pars = np.zeros(
+            (1, self.n_parameters, data.shape[1]), dtype=np.float32)
 
-    def optimize(self,
-                 min_nsteps=1000):
+        pars[:, 0, :] = mus
+        pars[:, 1, :] = sds
+        pars[:, 2, :] = amplitudes
+        pars[:, 3, :] = baselines
 
-        costs, parameters, predictions = super().optimize(min_nsteps=min_nsteps)
+        print(pars)
 
-        parameters.loc['sd'] = _softplus(parameters.loc['sd'])
-        if self.positive_amplitudes:
-            parameters.loc['amplitude'] = _softplus(
-                parameters.loc['amplitude'])
+        return pars
 
-        return costs, parameters, predictions
+    def simulate(self, parameters, paradigm, noise=1.):
 
-class GaussianReceptiveFieldFitter(EncodingModelFitter):
+        parameters[:, 1] = _inverse_softplus(parameters[:, 1])
+        parameters[:, 2] = _inverse_softplus(parameters[:, 2])
 
-    n_parameters = 4
-    parameter_labels = ['mu', 'sd', 'amplitude', 'baseline']
-
-    def __init__(self,
-                 paradigm,
-                 data,
-                 positive_amplitudes=True):
-
-        super().__init__(paradigm,
-                         data)
-
-        self.positive_amplitudes = positive_amplitudes
-
-        with self.graph.as_default():
-            self.mu = self.parameters[:, 0, :]
-            self.sd = tf.math.softplus(self.parameters[:, 1, :])
-
-            if self.positive_amplitudes:
-                self.amplitude_ = self.parameters[:, 2, :]
-                self.amplitude = tf.math.softplus(self.amplitude_)
-            else:
-                self.amplitude = self.parameters[:, 2, :]
-            self.baseline = self.parameters[:, 3, :]
-            self.norm = tfd.Normal(self.mu, self.sd)
-            self.predictions = self.baseline + \
-                self.norm.prob(self.paradigm) / \
-                self.norm.prob(self.mu) * self.amplitude
-
-            self.cost = tf.reduce_sum((self.predictions - self.data.values)**2)
-
-            self.optimizer = tf.train.AdamOptimizer()
-            self.train = self.optimizer.minimize(self.cost)
-
-    def optimize(self,
-                 min_nsteps=1000):
-
-        costs, parameters, predictions = super().optimize(min_nsteps=min_nsteps)
-
-        parameters.loc['sd'] = _softplus(parameters.loc['sd'])
-        if self.positive_amplitudes:
-            parameters.loc['amplitude'] = _softplus(
-                parameters.loc['amplitude'])
-
-        return costs, parameters, predictions
+        print(parameters)
+        data = super().simulate(parameters, paradigm, noise)
+        return data
 
 
 def _softplus(x):
     return np.log(1 + np.exp(x))
+
+
+def _inverse_softplus(x):
+    return np.log(np.exp(x) - 1)
+
+
+def norm(x, mu, sigma):
+    # Z = (2. * np.pi * sigma**2.)**0.5
+    kernel = tf.math.exp(-.5 * (x - mu)**2. / sigma**2)
+    return kernel
--- tests/test_decoders.py
+++ tests/test_decoders.py
@@ -1,17 +1,28 @@
-from braincoder.decoders import GaussianDecodingModel
+from braincoder.decoders import GaussianReceptiveFieldFitter
 from braincoder.generators import GaussianEncodingModel
 from braincoder.utils import get_rsq
 import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
 
-paradigm = np.repeat(np.arange(-10, 11), 10)
+
+model = GaussianReceptiveFieldFitter()
 n_voxels = 1000
-enc_model = GaussianEncodingModel(np.linspace(-5, 6, n_voxels, endpoint=True), np.ones(n_voxels) * 3)
 
-data = enc_model.simulate_data(paradigm, noise=.1)
-data = data * 10
+parameters = np.vstack((np.linspace(-10, 10, n_voxels), np.ones(n_voxels) * 5, np.ones(n_voxels) * 6, np.ones(n_voxels) * .2)).T
+parameters = parameters.astype(np.float32)
 
-dec_model = GaussianDecodingModel(paradigm, data)
+paradigm = np.repeat(np.arange(-10, 11, 1), 1)
+simulated_data = model.simulate(parameters, paradigm, noise=1)
 
-costs, pars, predictions = dec_model.optimize(min_nsteps=50000)
+dec_model = GaussianReceptiveFieldFitter()
+costs, pars, predictions = model.optimize(paradigm.copy(), simulated_data,
+        ftol=1e-12)
 
-r2 = get_rsq(data, predictions)
+r2 = get_rsq(simulated_data, predictions)
+
+
+plt.gcf()
+plt.plot(paradigm, predictions)
+plt.plot(paradigm, simulated_data, marker='+', lw=0, c='k')
+plt.show()
