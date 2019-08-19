0113da7d3ae5b5c6f58c8e0e827ff94937fa75cb
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2019-08-19 18:00:14 +0200

    Remove old test code

--- tests/test_identitymodel.py
+++ tests/test_identitymodel.py
@@ -1,4 +1,6 @@
-from braincoder.decoders import WeightedEncodingModel
+from braincoder.decoders import EncodingModel, IdentityModel
+
+# from braincoder.decoders import WeightedEncodingModel
 import numpy as np
 from braincoder.utils import get_rsq
 import matplotlib.pyplot as plt
@@ -6,39 +8,18 @@ import seaborn as sns
 
 n_timepoints = 100
 n_dimensions = 5
-n_voxels = 25
+n_voxels = 150
 
 paradigm = np.random.randn(n_timepoints, n_dimensions)
 weights = np.random.randn(n_dimensions, n_voxels)
 
-model = WeightedEncodingModel()
+model = IdentityModel()
 
-noise = np.arange(n_voxels) / n_voxels * 5.
-noise = 0.0
-data = model.simulate(paradigm, weights, noise=1.0)
-
-data += np.random.randn(len(data), 1).astype(np.float32) * 1
-
-costs = model.fit(paradigm, data)
+data = model.simulate(paradigm, weights=weights, noise=1.0)
+test = model.fit_weights(paradigm, data, l2_cost=0.0)
+data2 = model.simulate(paradigm, weights=weights, noise=1.0)
 
 predictions = model.get_predictions()
 r2 = model.get_rsq(data)
+r22 = model.get_rsq(data2)
 r = model.get_r(data)
-
-# data2 = model.simulate(paradigm, weights, noise=1.)
-# r22 = model.get_rsq(data2)
-
-# sns.distplot(r2)
-# sns.distplot(r22)
-
-# plt.plot(data.loc[:, :2])
-# plt.plot(predictions.loc[:, :2], ls='--')
-# plt.xlim(0, 10)
-plt.plot(costs)
-plt.show()
-
-# costs, weights_, predictions = model.optimize(paradigm, data, ftol=1e-13)
-# r2 = get_rsq(data, predictions)
-
-# ax = data.plot()
-# predictions.plot(ax=ax, marker='+', lw=0)
