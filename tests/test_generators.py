70bcd477913d670ac1e1676e0a05e7147fbe3ef3
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2019-08-19 18:00:51 +0200

    Remove old tests

--- tests/test_generators.py
+++
@@ -1,6 +0,0 @@
-from braincoder.generators import GaussianEncodingModel
-import numpy as np
-
-model = GaussianEncodingModel(np.arange(1, 11), np.ones(10))
-
-data = model.get_response_profile(np.arange(4, 7))
