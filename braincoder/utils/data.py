fae99b8f00e011ff8efd0b751c55d882d1751c07
Gilles de Hollander <gilles.de.hollander@gmail.com>
2025-08-05 14:10:47 +0200

    Use `importlib` rather than `pkg_resources`.

--- braincoder/utils/data.py
+++ braincoder/utils/data.py
@@ -1,35 +1,37 @@
-import pkg_resources
+from importlib.resources import files
 import numpy as np
 import pandas as pd
-from scipy import ndimage, io
-import tqdm
+from scipy import ndimage
 from nilearn.surface import load_surf_data
-from scipy.ndimage import zoom
 
 def load_szinte2024(resize_factor=1., best_voxels=None):
-
     data = {}
 
-    stream = pkg_resources.resource_stream(__name__, '../data/szinte2024/dm_gazecenter.npz')
-    data['stimulus'] = np.load(stream)['arr_0'].astype(np.float32)
+    base = files(__package__) / '../data/szinte2024'
 
-    data['grid_coordinates'] = pd.read_csv(pkg_resources.resource_stream(__name__, '../data/szinte2024/grid_coordinates.tsv'), sep='\t').astype(np.float32)
+    with (base / 'dm_gazecenter.npz').open('rb') as f:
+        data['stimulus'] = np.load(f)['arr_0'].astype(np.float32)
 
-    prf_pars = pd.read_csv(pkg_resources.resource_stream(__name__, '../data/szinte2024/gauss_parameters.tsv'), index_col='source', sep='\t').astype(np.float32)
+    with (base / 'grid_coordinates.tsv').open('rb') as f:
+        data['grid_coordinates'] = pd.read_csv(f, sep='\t').astype(np.float32)
+
+    with (base / 'gauss_parameters.tsv').open('rb') as f:
+        prf_pars = pd.read_csv(f, index_col='source', sep='\t').astype(np.float32)
 
     if resize_factor != 1.:
         data['stimulus'] = np.array([ndimage.zoom(d, 1./resize_factor) for d in data['stimulus']]).astype(np.float32)
 
-        tmp_x = data['grid_coordinates'].set_index(pd.MultiIndex.from_frame(data['grid_coordinates'] ))['x']
-        tmp_y = data['grid_coordinates'].set_index(pd.MultiIndex.from_frame(data['grid_coordinates'] ))['y']
+        tmp_x = data['grid_coordinates'].set_index(pd.MultiIndex.from_frame(data['grid_coordinates']))['x']
+        tmp_y = data['grid_coordinates'].set_index(pd.MultiIndex.from_frame(data['grid_coordinates']))['y']
 
         new_x = ndimage.zoom(tmp_x.unstack('y'), 1./resize_factor)
         new_y = ndimage.zoom(tmp_y.unstack('y'), 1./resize_factor)
 
-        # y needs to be flipped because of how stack and ravel works...
-        data['grid_coordinates'] = pd.DataFrame({'x':new_x.ravel(), 'y':new_y.ravel()[::-1]}).astype(np.float32)
+        data['grid_coordinates'] = pd.DataFrame({'x': new_x.ravel(), 'y': new_y.ravel()[::-1]}).astype(np.float32)
 
-    data['v1_timeseries'] = pd.read_csv(pkg_resources.resource_stream(__name__, '../data/szinte2024/mask-v1_bold.tsv.gz'), sep='\t', index_col=['time'], compression='gzip').astype(np.float32)
+    with (base / 'mask-v1_bold.tsv.gz').open('rb') as f:
+        data['v1_timeseries'] = pd.read_csv(f, sep='\t', index_col='time', compression='gzip').astype(np.float32)
+
     data['v1_timeseries'].columns = data['v1_timeseries'].columns.astype(int)
     data['tr'] = 1.317025
 
